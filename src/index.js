fd463d4a89713c2b13bb42e86ace0b047ae307bb
Alexandr Lisovoy <sashaliss18@gmail.com>
2024-04-14 16:00:22 +0500

    addStrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { Provider } from 'react-redux';
@@ -10,9 +10,10 @@ const rootContainer = document.getElementById('root');
 const root = createRoot(rootContainer);
 
 root.render(
-  // <StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  // </StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
+  </StrictMode>,
 );
